5d97acfcbd373b58cceefc9716f52d4fea1e9a49
lxm0524 <133079679+lxm0524@users.noreply.github.com>
2023-05-10 12:34:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oXvTW5lhCoZP9BkHCqEV2zx2c8e8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Qjbs4FrD8STLu88SzUAofOpd6lxKerkDcuzP3OSFyAs',
+      useTemplateId: '	zf_fEBJZWW-cXxH7lx2BvGHFz8Hui3wPz72KPWPkmVs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-18',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '	zf_fEBJZWW-cXxH7lx2BvGHFz8Hui3wPz72KPWPkmVs',
 
   CALLBACK_USERS: [
     {
